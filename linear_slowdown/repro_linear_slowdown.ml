cea8c4a7ef381feb00e2c050eec690a2b70ebc4a
Martynas Sinkievic <tasmartynas@gmail.com>
2022-05-11 19:39:12 +0100

    Simplify linear slowdown repro

--- linear_slowdown/repro_linear_slowdown.ml
+++ linear_slowdown/repro_linear_slowdown.ml
@@ -5,11 +5,12 @@
  *)
 let () =
   let core_count = try int_of_string @@ Sys.argv.(1) with _ -> 0 in
-  let arr1 = Array.make 1000_000 0.0 in
-  let arr2 = Array.make 1000_000 1.0 in
+  let arr = Array.make 1000_000 0.0 in
   let pool = Domainslib.Task.setup_pool ~num_additional_domains:core_count () in
   Domainslib.Task.run pool (fun () -> 
     let t = Mtime_clock.elapsed_ns () in
-    Array.iteri (fun i v -> arr2.(i) <- v) arr1;
+    for i=0 to Array.length arr - 1 do
+      arr.(i) <- arr.(i)
+    done;
     Printf.printf "Execution time: %fs\n" @@ Float.div (Int64.to_float @@ Int64.sub (Mtime_clock.elapsed_ns ()) t) 1000000000.;
   );
